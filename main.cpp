750840f987255002393e71c7d796bf6e6c492980
Khaltura <khalturin-yaroslav@mail.ru>
2024-04-13 13:51:22 +0700

    update main.cpp again

--- main.cpp
+++ main.cpp
@@ -18,6 +18,16 @@ int checkInput(int input_1)
     }
 }
 
+
+
+/**
+ * @brief функция возведения в степень input_1 числа которое занесено в vector
+ * 
+ * 
+ * @param input_1 - размер степени
+ * @param vec - число которое нужно возвести в степень занесённое в вектор
+ * @return std::vector<int> - число возведённое в степень
+ */
 std::vector<int> vozvedenie(int input_1, std::vector<int> vec)
 {
     for (int i = 1; i < input_1; ++i) {
@@ -53,22 +63,8 @@ int main() {
 
     std::vector<int> vec(1, 2); 
 
-    /*
-    for (int i = 1; i < input_1; ++i) {
-        int carry = 0;
-        for (int j = 0; j < vec.size(); ++j) {
-            int product = vec[j] * 2 + carry;
-            vec[j] = product % 10;
-            carry = product / 10;
-        }
-        while (carry > 0) {
-            vec.push_back(carry % 10);
-            carry /= 10;
-        }
-    }
-    */
    vec = vozvedenie(input_1, vec);
-   
+
    
     std::cout << "2 в степени " << input_1 << " равно ";
     for (int i = vec.size() - 1; i >= 0; --i) {
